698adb4d8332ca7f9663351d5afff6525b01b337
jimmygoel <goel.jimmy@gmail.com>
2024-05-07 12:32:22 +0530

    Add New Files

---
+++ Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; } = default!;
+        public List<ShoppingCartItem> item { get; set; } = new();
+        public decimal TotalPrice => item.Sum(x => x.Quantity * x.Price);
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+        public ShoppingCart()
+        {
+                
+        }
+    }
+}
